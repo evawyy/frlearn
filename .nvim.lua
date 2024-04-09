ff23c20f51b9d7fce574c94563b113d9eb5798a5
yinya wang <evawyy@yinyas-MacBook-Air.local>
2024-04-09 12:56:45 +0800

    accentsinp

--- .nvim.lua
+++ .nvim.lua
@@ -1,8 +1,12 @@
 local ls = require("luasnip")
+local d = ls.dynamic_node
 local s = ls.snippet
 local t = ls.text_node
+local c = ls.choice_node
+local sn = ls.snippet_node
 local i = ls.insert_node
 local f = ls.function_node
+local autosnippet = ls.extend_decorator.apply(s, { snippetType = "autosnippet" })
 local extras = require("luasnip.extras")
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 local rep = extras.rep
@@ -12,6 +16,144 @@ local in_tipa = function()
 	local r, c = unpack(vim.api.nvim_win_get_cursor(0))
 	line = string.sub(line, 0, c)
 	return string.match(line, "\\myf?tipa{[^}]*$")
+end
+--Personal snippet setting for spechial characters in french alphebet
+--
+
+local accent = {
+	grave = 1,
+	circonflexe = 2,
+	aigu = 3,
+	trema = 4,
+	cedille = 5,
+	oe = 6,
+}
+local pattern = {
+	d = accent.grave,
+	f = accent.circonflexe,
+	s = accent.trema,
+	a = accent.aigu,
+	g = accent.cedille,
+	e = accent.oe,
+}
+local fralphebet = {
+	a = {
+		[accent.grave] = "à",
+		[accent.circonflexe] = "â",
+		[accent.trema] = "",
+		[accent.aigu] = "",
+		[accent.cedille] = "",
+	},
+	A = {
+		[accent.grave] = "À",
+		[accent.circonflexe] = "Â",
+		[accent.trema] = "",
+		[accent.aigu] = "",
+		[accent.cedille] = "",
+	},
+	e = {
+		[accent.grave] = "è",
+		[accent.circonflexe] = "ê",
+		[accent.trema] = "ë",
+		[accent.aigu] = "é",
+		[accent.cedille] = "",
+	},
+	E = {
+		[accent.grave] = "È",
+		[accent.circonflexe] = "Ê",
+		[accent.trema] = "Ë",
+		[accent.aigu] = "É",
+		[accent.cedille] = "",
+	},
+	u = {
+		[accent.grave] = "ù",
+		[accent.circonflexe] = "û",
+		[accent.trema] = "ü",
+		[accent.aigu] = "",
+		[accent.cedille] = "",
+	},
+	U = {
+		[accent.grave] = "Ù",
+		[accent.circonflexe] = "Û",
+		[accent.trema] = "Ü",
+		[accent.aigu] = "",
+		[accent.cedille] = "",
+	},
+	i = {
+		[accent.grave] = "",
+		[accent.circonflexe] = "î",
+		[accent.trema] = "ï",
+		[accent.aigu] = "",
+		[accent.cedille] = "",
+	},
+	I = {
+		[accent.grave] = "",
+		[accent.circonflexe] = "Î",
+		[accent.trema] = "Ï",
+		[accent.aigu] = "",
+		[accent.cedille] = "",
+	},
+	y = {
+		[accent.grave] = "ÿ",
+		[accent.circonflexe] = "ÿ",
+		[accent.trema] = "ÿ",
+		[accent.aigu] = "ÿ",
+		[accent.cedille] = "ÿ",
+	},
+	Y = {
+		[accent.grave] = "Ÿ",
+		[accent.circonflexe] = "Ÿ",
+		[accent.trema] = "Ÿ",
+		[accent.aigu] = "Ÿ",
+		[accent.cedille] = "Ÿ",
+	},
+	o = {
+		[accent.grave] = "ô",
+		[accent.circonflexe] = "ô",
+		[accent.trema] = "ô",
+		[accent.aigu] = "ô",
+		[accent.cedille] = "ô",
+		[accent.oe] = "œ",
+	},
+	O = {
+		[accent.grave] = "Ô",
+		[accent.circonflexe] = "Ô",
+		[accent.trema] = "Ô",
+		[accent.aigu] = "Ô",
+		[accent.cedille] = "Ô",
+		[accent.oe] = "Œ",
+	},
+	c = {
+		[accent.grave] = "ç",
+		[accent.circonflexe] = "ç",
+		[accent.trema] = "ç",
+		[accent.aigu] = "ç",
+		[accent.cedille] = "ç",
+	},
+	C = {
+		[accent.grave] = "Ç",
+		[accent.circonflexe] = "Ç",
+		[accent.trema] = "Ç",
+		[accent.aigu] = "Ç",
+		[accent.cedille] = "Ç",
+	},
+}
+local makesnip = function(_, snip)
+	local alphas = fralphebet[snip.captures[1]]
+	if not alphas then
+		return sn(nil, t(snip.captures[1] .. ";" .. snip.captures[2]))
+	end
+	local cap = alphas[pattern[snip.captures[2]]]
+	if cap and cap ~= "" then
+		return sn(nil, t(cap))
+	end
+	local choices = {}
+	for _, v in ipairs(alphas) do
+		if v ~= "" then
+			table.insert(choices, t(v))
+		end
+	end
+	return sn(nil, { c(1, choices) })
 end
 ls.add_snippets("tex", {
 	s(
@@ -56,6 +198,18 @@ ls.add_snippets("tex", {
 			i(2),
 		}),
 		{ condition = line_begin }
+	),
+	s(
+		{ trig = "//", snippetType = "autosnippet" },
+		fmta(
+			[[
+      \myftipa{<>}<>
+      ]],
+			{
+				i(1),
+				i(0),
+			}
+		)
 	),
 	s(
 		{ trig = "([%a]+)@", regTrig = true, snippetType = "autosnippet" },
@@ -116,4 +270,7 @@ ls.add_snippets("tex", {
 		}),
 		{}
 	),
+	s({ trig = "(%a);(%a)", wordTrig = false, regTrig = true, snippetType = "autosnippet" }, {
+		d(1, makesnip),
+	}),
 })
